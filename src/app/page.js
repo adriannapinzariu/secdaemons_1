bbada14cd875d1113714df81d6f36abcefd35e81
brandon-kong <kongbrandon0@gmail.com>
2023-09-17 17:34:52 -0500

    Added prettier

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   const borderColor = "#D4000A";
@@ -7,29 +7,29 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
       <div className="flex items-center justify-center">
-      <Image 
+        <Image
           src="/brand/logo.png"
           alt="Logo"
           className="max-w-full h-auto"
-          width={500}   
+          width={500}
           height={500}
         />
-        <Image 
+        <Image
           src="/bar.svg"
           alt="Bar"
           layout="fixed"
-          width={11}   
+          width={11}
           height={11}
           className="absolute left-0"
-          style={{ top: '71.5%', left: '26.5%' }}
+          style={{ top: "71.5%", left: "26.5%" }}
         />
-      </div>           
-      <div 
-          className="flex items-center justify-center bg-transparent mt-5 w-full h-16 border-blue-demon-red"
-          style={{
-            borderWidth: borderThickness,
-          }}
-        ></div>
+      </div>
+      <div
+        className="flex items-center justify-center bg-transparent mt-5 w-full h-16 border-blue-demon-red"
+        style={{
+          borderWidth: borderThickness,
+        }}
+      ></div>
     </main>
-  )
+  );
 }
--- src/components/layout/Navbar/variants/main.jsx
+++ src/components/layout/Navbar/variants/main.jsx
@@ -4,53 +4,36 @@ import { navigations, socials } from "..";
 import Link from "next/link";
 import { NavbarItem } from "../components";
 
-export default function MainNavbar () {
-    return (
-        <div className="w-full h-[80px] flex justify-around items-center fixed top-0 left-0">
-            <div id="nav-image-container" className={'cursor-pointer'}>
-                <Image
-                src="/brand/logo.png"
-                alt="Logo"
-                height={80}
-                width={100}
-                />
-            </div>
+export default function MainNavbar() {
+  return (
+    <div className="w-full h-[80px] px-8 md:px-0 flex justify-between md:justify-around items-center fixed top-0 left-0">
+      <div id="nav-image-container" className={"cursor-pointer"}>
+        <Image src="/brand/logo.png" alt="Logo" height={80} width={100} />
+      </div>
 
-            <div className={'h-full hidden items-center gap-4 text-md sm:flex'}>
-                {/* Iterate through navigations and render each element */}
+      <div className={"h-full hidden items-center gap-4 text-md md:flex"}>
+        {/* Iterate through navigations and render each element */}
 
-                {
-                    navigations.map((value, index) => (
-                        <NavbarItem 
-                        key={index}
-                        value={value}
-                        />
-                    ))
-                }
+        {navigations.map((value, index) => (
+          <NavbarItem key={index} value={value} />
+        ))}
 
-                {/* Iterate through socials and render each element */}
+        {/* Iterate through socials and render each element */}
 
-                {
-                    socials.map((value, index) => (
-                        <Link
-                        href={value.href}
-                        key={index}
-                        className={'text-inactive-txt hover:text-white transition-colors pointer-events-auto cursor-pointer'}
-                        >
-                            <Image
-                            src={value.src}
-                            alt={value.href}
-                            height={25}
-                            width={25}
-                            />
-                        </Link>
-                    ))
-                }
-            </div>
+        {socials.map((value, index) => (
+          <Link
+            href={value.href}
+            key={index}
+            className={
+              "text-inactive-txt hover:text-white transition-colors pointer-events-auto cursor-pointer"
+            }
+          >
+            <Image src={value.src} alt={value.href} height={25} width={25} />
+          </Link>
+        ))}
+      </div>
 
-            <div>
-                
-            </div>
-        </div>
-    )
+      <div className={"flex md:hidden"}></div>
+    </div>
+  );
 }
