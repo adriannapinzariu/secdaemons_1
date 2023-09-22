132ab7790e817b207c62cd63a869158c8a44fd3c
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-09-22 18:47:27 -0500

    Loading Bar Sizing

--- src/app/page.js
+++ src/app/page.js
@@ -8,22 +8,24 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
             <div className="flex items-center justify-center">
                 <Image src="/brand/logo.png" alt="Logo" className="max-w-full h-auto" width={500} height={500} />
+            </div>
+            <div
+                className="relative flex items-center justify-center bg-transparent mt-5 w-full h-16 border-blue-demon-red max-w-lg"
+                style={{
+                    borderWidth: borderThickness,
+                    borderColor: borderColor,
+                }}
+            >
+                {/* Loading Bar positioned inside the red box */}
                 <Image
                     src="/bar.svg"
                     alt="Bar"
                     layout="fixed"
-                    width={11}
-                    height={11}
-                    className="absolute left-0"
-                    style={{ top: '71.5%', left: '26.5%' }}
+                    width={16}
+                    height={16}
+                    className="absolute left-1.5 top-1/2 transform -translate-y-1/2"
                 />
             </div>
-            <div
-                className="flex items-center justify-center bg-transparent mt-5 w-full h-16 border-blue-demon-red"
-                style={{
-                    borderWidth: borderThickness,
-                }}
-            ></div>
         </main>
     );
 }
