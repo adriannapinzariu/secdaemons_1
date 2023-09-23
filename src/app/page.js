e8b10129ff561d4038ab1f2b42577b5420e0afbc
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-09-22 23:10:56 -0500

    FINALLY

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@
 import React, { useRef, useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from './styles/Home.module.css';
-
-
+import { useGlitch } from 'react-powerglitch';
+import LoadingBar from './LoadingBar';
 
 
 export default function Home() {
@@ -12,13 +12,24 @@ export default function Home() {
     const [barsVisible, setBarsVisible] = useState(0);
     const totalBars = 26;
     const imageWidth = 16;
+    const glitch = useGlitch();
 
-    // Incrementally show bars
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            glitch.startGlitch();
+        }, 1000);
+
+        return () => {
+            clearTimeout(timer);
+            glitch.stopGlitch();
+        };
+    }, []);
+
     useEffect(() => {
         if (barsVisible < totalBars) {
             const timeout = setTimeout(() => {
                 setBarsVisible(barsVisible + 1);
-            }, 35);  // Here, 100ms is the delay between showing each bar
+            }, 35);
 
             return () => clearTimeout(timeout);
         }
@@ -27,37 +38,11 @@ export default function Home() {
     const borderThickness = '5px';
 
     return (
-        
-
         <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
-            <div className="flex items-center justify-center">
-                <Image src="/brand/logo.png" alt="Logo" className="max-w-full h-auto" width={500} height={500} />
+            <div className="flex items-center justify-center relative">
+            <Image src="/brand/logo.png" ref={glitch.ref} alt="Logo" width={500} height={500} />
             </div>
-            <div
-                ref={containerRef}
-                className="relative flex items-center justify-center bg-transparent mt-5 w-full h-16 border-blue-demon-red max-w-543px"
-                style={{
-                    borderWidth: borderThickness,
-                    borderColor: borderColor,
-                }}
-            >
-  
-        {Array.from({ length: barsVisible }).map((_, idx) => (
-        <Image
-            key={idx}
-            src="/bar.svg"
-            alt="Bar"
-            layout="fixed"
-            width={15}
-            height={16}
-            className={`absolute top-1/2 transform -translate-y-1/2`}
-            style={{ left: `${(9 + (idx) * (imageWidth + 4))}px` }}
-        />
-    ))}
-      
-            </div>
+            <LoadingBar glitchRef={glitch.ref} />
         </main>
     );
 }
-
-
